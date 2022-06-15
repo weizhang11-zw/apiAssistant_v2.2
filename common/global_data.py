df10dbcbec213b583f03e7b43be66c6ada06cdaf
weizhang11-zw <58303130+weizhang11-zw@users.noreply.github.com>
2022-06-15 14:40:06 +0800

    Update global_data.py

--- common/global_data.py
+++ common/global_data.py
@@ -13,6 +13,3 @@ class Data(object):
     day_after_tomorrow = t.get_the_day_after_tomorrow()
     nowtime_greater_than_now = t.get_nowtime_greater_than_now()
     yesterday_time = t.get_yesterday()
-    xiaokang_token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJwaG9uZSI6IjE3NjIxNTI1Mzg3IiwiZXhwVGltZSI6MzE1MzYwMDAwMDAsImFjY291bnRObyI6IkdNMjAyMjA1MjAwOTU2MjYwMDAwMDAwMzA1IiwicGxhdFR5cGUiOiI0IiwidXNlck5hbWUiOiJnbV81Mzg3IiwiZXhwIjoxNjg0NTYzOTc3LCJ1c2VySWQiOiIxMzMwMSJ9.AAKPonKC7Y4ZfZ3x6qitTKVqsQCjb921IqfXMI_FgDO9K62I7Vjz_3cHPOJ00_R_8c5YWu_l-ZKtcuSESX1SBg"
-    user_name = "17621525387"
-    password = "541925"
